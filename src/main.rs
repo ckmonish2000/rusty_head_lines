c495d934603e774332588795144b4ea12928ec72
monish karunakaran <ckmonish2000@gmail.com>
2022-03-02 05:00:21 +0530

    working parse to data

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
-use colored::*;
+use serde::{Deserialize, Serialize};
 use ureq::{get};
 
+#[derive(Serialize, Deserialize,Debug)]
+struct Article{
+    // name:String,
+    title:String,
+}
+
+#[derive(Serialize, Deserialize,Debug)]
+struct Articles{
+    articles: Vec<Article>,
+}
+
+
 fn main() {
-    get_news();
+    get_news().unwrap();
    }
+
 
 
 
@@ -11,13 +24,15 @@ fn main() {
 fn get_news() -> Result<(),ureq::Error> {
     let path ="https://newsapi.org/v2/top-headlines?country=us&apiKey=5b9d5eb78659499fa9b0e4d2fee0059a";
     
-    let body: serde_json::Value = get(path)
+    let body: Articles = get(path)
     .call()?
     .into_json()?;
 
+    
+
     // let body2 = serde_json::from_str(body);
 
-    println!("{}",body["articles"]);
+    println!("{:#?}",body);
 
     Ok(())
 }
